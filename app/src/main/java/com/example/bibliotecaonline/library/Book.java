9d7e107312c9bbfdac5669dda1152add12065b30
SabinaChiriac <chiriac.sabina@yahoo.com>
2021-06-19 19:59:06 +0300

    fixed some error and added book gender and book classes

---
+++ app/src/main/java/com/example/bibliotecaonline/library/Book.java
@@ -0,0 +1,14 @@
+package com.example.bibliotecaonline.library;
+
+import com.example.bibliotecaonline.library.Books_Gendere;
+public class Book {
+
+    private String Title;
+    private String Author;
+    private Books_Gendere gender;
+    private  String Publishing_year;
+    private String Description;
+
+
+
+}
---
+++ app/src/main/java/com/example/bibliotecaonline/library/Books_Gendere.java
@@ -0,0 +1,11 @@
+package com.example.bibliotecaonline.library;
+
+public enum Books_Gendere {
+    BIBLIOGRAPY,
+    FANTASY,
+    FICTION,
+    POETRY,
+    COMICS,
+    ROMANCE
+
+}
