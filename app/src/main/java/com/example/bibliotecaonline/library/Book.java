a87d8d3e93e436772acc068fdfc435dc4f0c3c4f
SabinaChiriac <chiriac.sabina@yahoo.com>
2021-06-23 18:08:47 +0300

    Added activity main menu

--- app/src/main/java/com/example/bibliotecaonline/library/Book.java
+++ app/src/main/java/com/example/bibliotecaonline/library/Book.java
@@ -5,10 +5,19 @@ public class Book {
 
     private String Title;
     private String Author;
-    private Books_Gendere gender;
+    private Books_Gendere Gender;
     private  String Publishing_year;
     private String Description;
 
+    public Books_Gendere getGender() { return Gender; }
+    public String getAuthor() { return Author; }
+    public String getDescription() { return Description;}
+    public String getPublishing_year() { return Publishing_year; }
+    public String getTitle() { return Title; }
+
+    public void setAuthor(String author) { this.Author = author; }
+    public void  setTitle(String title){ this.Title=title;}
+    public void setPublishing_year(String publishing_year){this.Publishing_year=publishing_year;}
 
 
 }
