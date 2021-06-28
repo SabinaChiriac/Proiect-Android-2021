bbee42bc3a5ae27fe036785270c0ad8e14b15e2c
SabinaChiriac <chiriac.sabina@yahoo.com>
2021-06-28 14:48:29 +0300

    created nav_graph and added back actions from one fragment to another

--- app/src/main/java/com/example/bibliotecaonline/activities/fragments/LibraryInfoPage.kt
+++ app/src/main/java/com/example/bibliotecaonline/activities/fragments/LibraryInfoPage.kt
@@ -2,10 +2,15 @@ package com.example.bibliotecaonline.activities.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.bibliotecaonline.R
+
+
 class LibraryInfoPage : Fragment() {
 
     override fun onCreateView(
@@ -14,5 +19,18 @@ class LibraryInfoPage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_library_info_page, container, false)
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       when(item.getItemId()){
+           android.R.id.home->
+               findNavController().navigate(R.id.action_LibraryInfo_to_MainMenu)
+       }
+        return true
     }
 }
